31b8bfa1f22095d2592bf7abc61ba21e05b2c389
naufalraism <naufalraismajid@gmail.com>
2023-02-09 22:51:28 +0700

    Update footer dan img png footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -12,13 +12,13 @@
             <div class="col-sm">
                <h3>FOLLOW US</h3>
                <ul style="margin-left: 100px; text-align: left">
-                  <li><a href="https://www.facebook.com/"><i><img src="{{ asset('images/facebook.png') }}" class="rounded-circle" height="30px" width="30px" alt="Facebook.png"></i></a>
+                  <li><a href="https://www.facebook.com/"><i><img src="{{ asset('images/facebooklogo.png') }}" class="rounded-circle" height="30px" width="30px" alt="Facebook.png"></i></a>
                      <a href="https://www.facebook.com/" style="text-decoration: none; font: bold 16px sans-serif; margin-left: 10px; color: white"> Facebook</a><p></p>
                   </li>
-                  <li><a href="https://www.twitter.com/"><i><img src="{{ asset('images/twitter.png') }}" class="rounded-circle" height="30px" width="30px" alt="Twitter.png"></i></a>
+                  <li><a href="https://www.twitter.com/"><i><img src="{{ asset('images/twitterlogo.png') }}" class="rounded-circle" height="30px" width="30px" alt="Twitter.png"></i></a>
                      <a href="https://www.twitter.com/" style="text-decoration: none; font: bold 16px sans-serif; margin-left: 10px; color: white"> Twitter</a><p></p>
                   </li>
-                  <li><a href="https://www.instagram.com/"><i><img src="{{ asset('images/instagram.png') }}" class="rounded-circle" height="30px" width="30px" alt="Instagram.png"></i></a>
+                  <li><a href="https://www.instagram.com/"><i><img src="{{ asset('images/instagramlogo.png') }}" class="rounded-circle" height="30px" width="30px" alt="Instagram.png"></i></a>
                      <a href="https://www.instagram.com/" style="text-decoration: none; font: bold 16px sans-serif; margin-left: 10px; color: white"> Instagram</a><p></p>
                   </li>
                </ul>
@@ -29,7 +29,6 @@
                <p class="text-justify">Electrify is a place that provides various kinds of electronic goods that
                   you're looking for
                   starting from laptops to keyboards.</p>
-
             </div>
          </div>
          <hr>
