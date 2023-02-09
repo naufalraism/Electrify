cea96de92cf5c2f6431d8d5c2846e9a523f3c6d2
naufalraism <naufalraismajid@gmail.com>
2023-02-09 22:46:52 +0700

    Update search navbar blade, product controller, route web, search-result

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,30 +1,39 @@
+
+
 <footer class="footer text-center ww-100">
    <div class="text-center p-3 bg-dark text-white" style="min-height:50px;">
       <div class="container">
          <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm">
+               <h3>ELECTRIFY</h3>
+
+            </div>
+
+            <div class="col-sm">
+               <h3>FOLLOW US</h3>
+               <ul style="margin-left: 100px; text-align: left">
+                  <li><a href="https://www.facebook.com/"><i><img src="{{ asset('images/facebook.png') }}" class="rounded-circle" height="30px" width="30px" alt="Facebook.png"></i></a>
+                     <a href="https://www.facebook.com/" style="text-decoration: none; font: bold 16px sans-serif; margin-left: 10px; color: white"> Facebook</a><p></p>
+                  </li>
+                  <li><a href="https://www.twitter.com/"><i><img src="{{ asset('images/twitter.png') }}" class="rounded-circle" height="30px" width="30px" alt="Twitter.png"></i></a>
+                     <a href="https://www.twitter.com/" style="text-decoration: none; font: bold 16px sans-serif; margin-left: 10px; color: white"> Twitter</a><p></p>
+                  </li>
+                  <li><a href="https://www.instagram.com/"><i><img src="{{ asset('images/instagram.png') }}" class="rounded-circle" height="30px" width="30px" alt="Instagram.png"></i></a>
+                     <a href="https://www.instagram.com/" style="text-decoration: none; font: bold 16px sans-serif; margin-left: 10px; color: white"> Instagram</a><p></p>
+                  </li>
+               </ul>
+            </div>
+
+            <div class="col-sm">
                <h3>ABOUT US</h3>
                <p class="text-justify">Electrify is a place that provides various kinds of electronic goods that
                   you're looking for
                   starting from laptops to keyboards.</p>
-            </div>
-
-            <div class="col-sm-4">
-               <h3>FOLLOW US</h3>
-               <ul>
-                  <li>Facebook</li>
-                  <li>Twitter</li>
-                  <li>Instagram</li>
-               </ul>
-            </div>
-
-            <div class="col-sm-4">
-               <h3>ELECTRIFY</h3>
 
             </div>
          </div>
          <hr>
-         <p classs="copyright">©Electrify {{ now()->year }}</p>
+         <p class="copyright">©Electrify {{ now()->year }}</p>
       </div>
    </div>
 </footer>
